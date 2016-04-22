49ec6d174210179b4f43407100bca9f37a493e5b
trigil <gillard.tristan@gmail.com>
2016-04-22 15:14:37 +0200

    modif python afficheur self et image.load()

--- src/Maestro Box/chargeur partition v3/afficheur.py
+++ src/Maestro Box/chargeur partition v3/afficheur.py
@@ -27,7 +27,7 @@ class afficheur :
 
   def lire(self, m, mesure_debut_lecture, passage_debut_lecture, mesure_fin_lecture, passage_fin_lecture):
 
-    afficher_logo()
+    self.afficher_logo()
 
     gc.disable()
 
@@ -51,13 +51,13 @@ class afficheur :
     scheduler.enter(temps_affichage_logo, 1, wait, ())
     scheduler.run()
 
-    afficher_decompte(descripteur["temps_par_mesure"], descripteur["tempo"], scheduler)
+    self.afficher_decompte(descripteur["temps_par_mesure"], descripteur["tempo"], scheduler)
 
     while(descripteur["mesure_courante"], descripteur["passage_reprise_courant"]) != (mesure_fin_lecture + 1, passage_fin_lecture):
       if (str(descripteur["mesure_courante"]) + '.' + str(descripteur["passage_reprise_courant"])) in map_mesures_modif :
         descripteur.update(map_mesures_modif[str(descripteur["mesure_courante"]) + '.' + str(descripteur["passage_reprise_courant"])])
       if "mesure_non_lue" not in descripteur :
-        afficher(descripteur, scheduler)
+        self.afficher(descripteur, scheduler)
       if "prochaine_mesure" in descripteur:
         descripteur["mesure_courante"] = descripteur["prochaine_mesure"]
       else:
@@ -65,7 +65,7 @@ class afficheur :
       if "prochain_passage" in descripteur:
         descripteur["passage_reprise_courant"] = descripteur["prochain_passage"]
 
-    afficher_fin()
+    self.afficher_fin()
 
     gc.enable()
 
@@ -74,12 +74,12 @@ class afficheur :
 
     tempo_en_seconde = 60 / float(descripteur["tempo"])
 
-    scheduler.enter(0,3,afficher_mesure,(descripteur["mesure_courante"],))
-    scheduler.enter(0,3,afficher_passage,(descripteur["passage_reprise_courant"],))
+    scheduler.enter(0,3,self.afficher_mesure,(descripteur["mesure_courante"],))
+    scheduler.enter(0,3,self.afficher_passage,(descripteur["passage_reprise_courant"],))
 
     for t in range(1, descripteur["temps_par_mesure"] + 1):
 
-      scheduler.enter((t - 1) * tempo_en_seconde, 1, afficher_temps, (descripteur["temps_par_mesure"], t))
+      scheduler.enter((t - 1) * tempo_en_seconde, 1, self.afficher_temps, (descripteur["temps_par_mesure"], t))
 
       if ("temps_debut_intensite_"+str(t) in descripteur):
         if (descripteur["nb_temps_intensite_"+str(t)] > 0):
@@ -98,10 +98,10 @@ class afficheur :
         del descripteur["intensite_"+str(t)]
         del descripteur["temps_debut_intensite_"+str(t)]
         del descripteur["nb_temps_intensite_"+str(t)]
-        scheduler.enter((t - 1) * tempo_en_seconde, 1, afficher_intensite, (descripteur["intensite_courante"],))
+        scheduler.enter((t - 1) * tempo_en_seconde, 1, self.afficher_intensite, (descripteur["intensite_courante"],))
 
       if ("temps_alerte_" + str(t) in descripteur):
-        scheduler.enter((t - 1) * tempo_en_seconde, 1, afficher_alerte, (descripteur["couleur_alerte_" + str(t)],))
+        scheduler.enter((t - 1) * tempo_en_seconde, 1, self.afficher_alerte, (descripteur["couleur_alerte_" + str(t)],))
         if (descripteur["couleur_alerte_" + str(t)] != -1) and ("temps_alerte_" + str(t+1) not in descripteur):
           descripteur["temps_alerte_" + str(t+1)] = True
           descripteur["couleur_alerte_" + str(t+1)] = -1
@@ -109,7 +109,7 @@ class afficheur :
         del descripteur["couleur_alerte_" + str(t)]
 
       if ("temps_debut_armature_" + str(t) in descripteur):
-        scheduler.enter((t - 1) * tempo_en_seconde, 1, afficher_armature, (descripteur["alteration_" + str(t)],))
+        scheduler.enter((t - 1) * tempo_en_seconde, 1, self.afficher_armature, (descripteur["alteration_" + str(t)],))
         del descripteur["temps_debut_armature_" + str(t)]
         del descripteur["alteration_" + str(t)]
 
@@ -120,15 +120,18 @@ class afficheur :
 
   def afficher_logo(self):
     image = Image.open(chemin_images + 'ema logo.png')
+    image.load()
     self.matrix.SetImage(image.im.id, 0, 0)
 
 
   def afficher_decompte(self, temps_par_mesure, tempo, scheduler):
     tempo_en_seconde = 60 / float(tempo)
     image = Image.open(chemin_images + '64*32_black.png')
+    image.load()
     scheduler.enter(t * tempo_en_seconde, 1, self.matrix.SetImage, (image.im.id, 0, 0))
     for t in range(temps_par_mesure):
       image = Image.open(chemin_images + str(temps_par_mesure - t) + '.png')
+      image.load()
       scheduler.enter(t * tempo_en_seconde, 1, self.matrix.SetImage, (image.im.id, 0, 0))
     scheduler.enter(temps_par_mesure * tempo_en_seconde, 1, wait, ())
     scheduler.run()
@@ -141,11 +144,13 @@ class afficheur :
 
   def afficher_fin(self):
     image = Image.open(chemin_images + 'fin.png')
+    image.load()
     self.matrix.SetImage(image.im.id, 0, 0)
 
 
   def afficher_temps(self, temps_par_mesure, temps_courant):
     image = Image.open(chemin_images + 'a' + str(temps_par_mesure)+'_'+str(temps_courant)+'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_temps[0], pos_temps[1])
 
 
@@ -161,23 +166,28 @@ class afficheur :
       image = Image.open(chemin_images + '8*8_black.png')
     else:
       image = Image.open(chemin_images + 'mesure' + str(centaine)+'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_centaine[0], pos_centaine[1])
     if dizaine == -1:
       image = Image.open(chemin_images + '8*8_black.png')
     else:
       image = Image.open(chemin_images + 'mesure' + str(dizaine)+'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_dizaine[0], pos_dizaine[1])
     image = Image.open(chemin_images + 'mesure' + str(unite)+'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_unite[0], pos_unite[1])
 
 
   def afficher_passage(self, passage):
     image = Image.open(chemin_images + 'mesure' + str(passage)+'.png')# remplacer mesure par passage quand il y aura des images specifiques
+    image.load()
     self.matrix.SetImage(image.im.id, pos_passage[0], pos_passage[1])
 
 
   def afficher_intensite(self, intensite):
     image = Image.open(chemin_images + 'intensite' + str(int(round(intensite)))+'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_intensite[0], pos_intensite[1])
 
 
@@ -186,6 +196,7 @@ class afficheur :
       image = Image.open(chemin_images + '8*8_black.png')
     else:
       image = Image.open(chemin_images + partie +'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_partie[0], pos_partie[1])
 
 
@@ -194,6 +205,7 @@ class afficheur :
       image = Image.open(chemin_images + '8*8_black.png')
     else:
       image = Image.open(chemin_images + 'alerte' + str(alerte) +'.png')
+    image.load()
     self.matrix.SetImage(image.im.id, pos_alerte[0], pos_alerte[1])
 
 
